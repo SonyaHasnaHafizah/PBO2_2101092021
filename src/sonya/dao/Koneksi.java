1c7f1a2f1e9a1b174874382b4aec9c4a10be8637
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2023-03-09 15:50:14 +0700

    Create Koneksi.java

---
+++ src/sonya/dao/Koneksi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sonya.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author SONYA HASNA HAFIZAH
+ */
+public class Koneksi {
+    private String url = "jdbc:mysql://localhost/pbo2_2101092021";
+    private String username = "root";
+    private String password = "";
+    
+    public Connection getKoneksi() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(url, username,password);
+    }
+    
+    
+}
