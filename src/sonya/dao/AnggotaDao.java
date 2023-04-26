24655ca93a35993407cc956e02c749a0dd8031e6
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2023-04-26 11:47:05 +0700

    Update AnggotaDao.java

--- src/sonya/dao/AnggotaDao.java
+++ src/sonya/dao/AnggotaDao.java
@@ -14,9 +14,10 @@ import sonya.model.Anggota;
  * @author SONYA HASNA HAFIZAH
  */
 public interface AnggotaDao{
-    void insert(Connection con, Anggota anggota) throws Exception;
-    public void update(Connection con,sonya.model.Anggota anggota) throws Exception;
-    public void delete(Connection con,sonya.model.Anggota anggota) throws Exception;
-    public sonya.model.Anggota getAnggota(Connection con,String kode) throws Exception;
-    public List<sonya.model.Anggota> getAllAnggota(Connection con) throws Exception;
+   public void insert ( Connection con, Anggota anggota ) throws Exception;
+   public void update (Connection con,Anggota anggota ) throws Exception;
+   public void delete (Connection con,Anggota anggota ) throws Exception;
+   public Anggota getAnggota (Connection con,String kode) throws Exception;
+   public Anggota getAllAnggota(Connection con ,String kode) throws Exception;
+   public List<Anggota> getAllAnggota(Connection con ) throws Exception;
 }
