0a4799511e11d078e6a7282af720447ce3abdcb1
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2023-06-14 12:45:26 +0700

    Create Pengembalian.java

---
+++ src/sonya/model/Pengembalian.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sonya.model;
+
+/**
+ *
+ * @author Axioo
+ */
+public class Pengembalian {
+    private String kodeanggota;
+    private String kodebuku;
+    private String tglpinjam;
+    private String tgldikembalikan;
+    private int terlambat;
+    private double denda;
+    
+    public Pengembalian(){
+        
+    }
+
+    public Pengembalian(String kodeanggota, String kodebuku, String tglpinjam, String tgldikembalikan, int terlambat, double denda) {
+        this.kodeanggota = kodeanggota;
+        this.kodebuku = kodebuku;
+        this.tglpinjam = tglpinjam;
+        this.tgldikembalikan = tgldikembalikan;
+        this.terlambat = terlambat;
+        this.denda = denda;
+    }
+
+    public String getKodeanggota() {
+        return kodeanggota;
+    }
+
+    public void setKodeanggota(String kodeanggota) {
+        this.kodeanggota = kodeanggota;
+    }
+
+    public String getKodebuku() {
+        return kodebuku;
+    }
+
+    public void setKodebuku(String kodebuku) {
+        this.kodebuku = kodebuku;
+    }
+
+    public String getTglpinjam() {
+        return tglpinjam;
+    }
+
+    public void setTglpinjam(String tglpinjam) {
+        this.tglpinjam = tglpinjam;
+    }
+
+    public String getTgldikembalikan() {
+        return tgldikembalikan;
+    }
+
+    public void setTgldikembalikan(String tgldikembalikan) {
+        this.tgldikembalikan = tgldikembalikan;
+    }
+
+    public int getTerlambat() {
+        return terlambat;
+    }
+
+    public void setTerlambat(int terlambat) {
+        this.terlambat = terlambat;
+    }
+
+    public double getDenda() {
+        return denda;
+    }
+
+    public void setDenda(double denda) {
+        this.denda = denda;
+    }
+    
+    
+    
+}
