eea82c4b90a8ca4dc4a00677e60cf6f6c087504f
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2023-06-14 12:48:21 +0700

    Create Koneksi.java

---
+++ src/uts_b/dao/Koneksi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_b.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Axioo
+ */
+public class Koneksi {
+    private String username = "root";
+    private String password ="";
+    private String url;
+    
+    public Connection getKoneksi () throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
