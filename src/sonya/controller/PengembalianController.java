178648bfaa01c307b8b2497a9814f72848bb1586
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2023-07-05 12:33:14 +0700

    Update PengembalianController.java

--- src/sonya/controller/PengembalianController.java
+++ src/sonya/controller/PengembalianController.java
@@ -5,10 +5,41 @@
  */
 package sonya.controller;
 
+import java.sql.Connection;
+import sonya.dao.AnggotaDao;
+import sonya.dao.AnggotaDaoImpl;
+import sonya.dao.BukuDao;
+import sonya.dao.BukuDaoImpl;
+import sonya.dao.Koneksi;
+import sonya.dao.PeminjamanDao;
+import sonya.dao.PeminjamanDaoImpl;
+import sonya.dao.PengembalianDao;
+import sonya.dao.PengembalianDaoImpl;
+import sonya.model.Pengembalian;
+import sonya.view.FormPengembalian;
+
 /**
  *
  * @author Axioo
  */
 public class PengembalianController {
+    FormPengembalian formpengembalian;
+    AnggotaDao anggotaDao;
+    BukuDao bukuDao;
+    PeminjamanDao peminjamanDao;
+    PengembalianDao pengembalianDao;
+    Pengembalian pengembalian;
+    Connection connection;
+    
+    public PengembalianController(FormPengembalian formpengembalian){
+        //this.formPengembalian = FormPengembalian();
+        anggotaDao = new AnggotaDaoImpl();
+        bukuDao = new BukuDaoImpl();
+        peminjamanDao = new PeminjamanDaoImpl();
+        pengembalianDao = new PengembalianDaoImpl();
+        Koneksi k = new Koneksi();
+        //connection = k.getConnection();
+        
+    }
     
 }
