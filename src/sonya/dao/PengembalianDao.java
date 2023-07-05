e8f552f3f409ff4a6a65769449114f4ad0d00528
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2023-07-05 12:33:25 +0700

    Update PengembalianDao.java

--- src/sonya/dao/PengembalianDao.java
+++ src/sonya/dao/PengembalianDao.java
@@ -17,9 +17,9 @@ public interface PengembalianDao {
     public void insert(Connection con, Pengembalian pengembalian) throws Exception;
     public void update(Connection con, Pengembalian pengembalian) throws Exception;
     public void delete(Connection con,  Pengembalian pengembalian) throws Exception;
-    public Pengembalian getPengembalian(Connection con, String kodeanggota, String kodebuku, String tglpinjam) throws Exception;
+    public Pengembalian getPengembalian(Connection con, String kodeanggota, String kodebuku, String tglpinjam, String tgldikembalikan, String terlambat, String denda) throws Exception;
     public List<Pengembalian> getAllPengembalian(Connection con) throws Exception;
-    double getSelisihTanggal(Connection con, String tgl1, String tgl2) throws Exception;
+    public int getSelisihTanggal(Connection con, String tgl1, String tgl2) throws Exception;
     
     //select datediff()'2023-01-03','2023-01-01') as hasil;
 }
